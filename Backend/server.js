cbd26e6631fe20d32d668542817deaa3103daf81
Heeba Khan <heebak5@gmail.com>
2023-05-14 02:41:47 +0530

    Added server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -8,11 +7,6 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5001;
 
-const connect = mongoose.connect;
-connect.once("open", () => {
-  console.log("mongodb database connection established successfully!!");
-});
-
-const transactionRouter = require("./routes/trasactionhist");
-const registrations = require("./routes/register");
-const contactRouter = require("./routes/contact");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
